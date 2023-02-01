94c9135107f8115bf2faac697ec3c63ceaedc413
domieee <99103235+domieee@users.noreply.github.com>
2023-02-01 15:17:52 +0100

    init, added js

---
+++ assets/js/main.js
@@ -0,0 +1,28 @@
+let languages = [
+    "JavaScript",
+    "Python",
+    "Java",
+    "Ruby",
+    "PHP",
+    "C++",
+    "CSS",
+    "C#",
+    "Go",
+    "C",
+    "TypeScript",
+    "Swift"
+];
+
+// In dieser Übung lernen wir, wie man Arrays alphabetisch sortiert.
+//      - Sortiere die Programmiersprachen alphabetisch.
+//      - Der Code für das Array steht im Kommentarbereich.
+//      - Deklariere die Funktion Sortierung.
+//      - Verwende den Befehl sort(), um die Aufgabe zu erfüllen.
+//      - Ruf die Funktion Sortierung auf.
+//      - Überprüfe den Erfolg in der Konsole.
+
+function sortArray() {
+    console.log(languages.sort());
+}
+
+sortArray();
